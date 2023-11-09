8d11b14262e3b6b4d44d70c41b8c1baaa1600f86
MasD91 <>
2023-11-09 20:22:09 +0000

    rendering data 2

--- todo/admin.py
+++ todo/admin.py
@@ -1,9 +1,7 @@
 from .models import Item
 from django.contrib import admin
-<< << << < HEAD
-# Register your models here.
-admin.site.register(Item)
-== == == =
 
 # Register your models here.
->>>>>> > refs/remotes/origin/main
+admin.site.register(Item)
+
+# Register your models here.
