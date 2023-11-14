ec518dce3c2d287d3cef29a06875fa7750685430
MasD91 <>
2023-11-14 18:41:47 +0000

    prepared to deploy to heroku

--- django_todo/wsgi.py
+++ django_todo/wsgi.py
@@ -13,6 +13,5 @@ from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_todo.settings')
-os.environ.setdefault("DATABASE_URL", "my_copied_database_url")
 
 application = get_wsgi_application()
